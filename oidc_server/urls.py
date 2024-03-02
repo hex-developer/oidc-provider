28203b718dfa2eb764b4ce1c72133f6dc4130ba8
hex-developer <77530549+hex-developer@users.noreply.github.com>
2024-03-02 14:29:36 +0200

    Still trying

--- oidc_server/urls.py
+++ oidc_server/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
     path(
         "accounts/login/",
-        auth_views.LoginView.as_view(template_name="login.html"),
+        csrf_exempt(auth_views.LoginView.as_view(template_name="login.html")),
         name="login",
     ),
     path(
